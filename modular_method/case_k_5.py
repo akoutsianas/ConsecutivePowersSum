a6bd45ca4a5d1250ae830c06ae27d6ff80650364
Angelos Koutsianas <koutsis.jr@gmail.com>
2024-09-05 15:13:37 +0300

    Case k=5: Fix bugs in the elimination step with traces of Frobenius. One newform is noteliminated.

--- modular_method/case_k_5.py
+++ modular_method/case_k_5.py
@@ -7,26 +7,28 @@ def modular_method_k_5(primes_bound=30):
       A list of small primes over the modular method fails
   """
 
-  E = lambda t: EllipticCurve([0, 5*(20*t+3), 0, 5*(5*(20*t + 3)**2 - 1)/4, 0])
+  y2p = lambda t: QQ(5*(10*t*(t+1) + 3)**2 - 1)/4
+  E = lambda t: EllipticCurve([0, 5*(10*t*(t + 1) + 3), 0, 5*y2p(t), 0])
   Snew = Newforms(Gamma0(200), 2)
   
+  problematic_fnew = []
   for newf in Snew:
     Pqs = []
     for q in primes(primes_bound+1):
       if q not in [2,5]:
-        print("q: {}".format(q))
+        # print("q: {}".format(q))
         prodq = 1
         aqfnew = newf[q]
         for t0 in range(q):
           # print("t0: {}".format(t0))
-          y2p = (5*(20*ZZ(t0) + 3)**2 - 1)/4
-          if y2p % q != 0:
-            # print("Et0: {}".format(E(ZZ(t0))))
+          if y2p(ZZ(t0)) % q != 0:
             aqE = q + 1 - E(ZZ(t0)).reduction(q).order()
             prodq *= (aqE - aqfnew)
           else:
             prodq *= (aqfnew**2 - (q + 1)**2)
-        print("prodq: {}".format(prodq))
+        # print("prodq: {}\n".format(prodq))
         Pqs.append(prodq)
+    if len([c for c in Pqs if c != 0]) == 0:
+      problematic_fnew.append(newf)
     print("Pqs: {}".format(Pqs))
-        
+  return problematic_fnew
